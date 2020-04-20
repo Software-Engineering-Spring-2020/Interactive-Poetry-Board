c28a441af3b6cb86c4b8a6dc91753004499599be
Isaac Bake <isaac@blake.earth>
2020-04-20 16:57:02 -0400

    Add yesterdaysNews.ts

---
+++ src/app/yesterdaysNews.ts
@@ -0,0 +1,364 @@
+export const ids = {
+    "34a": {
+        "word": "A reverse fountain of youth",
+        "clue": "the thick-skinned poetry of the comfortable",
+        "startX": 0,
+        "startY": 0,
+    },
+
+    "14d": {
+        "word": "Agency",
+        "clue": "is a broken record. Spin me round baby.",
+        "startX": 0,
+        "startY": 0,
+    },
+
+    "19a": {
+        "word": "All symmetry",
+        "clue": "awaits the event that will dislodge it in a single blow",
+        "startX": 0,
+        "startY": 0,
+    },
+
+    "30a": {
+        "word": "American dream",
+        "clue": "a seductive invitation to fuse one’s private fortune with that of the nation",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "15a": {
+        "word": "By not paying attention",
+        "clue": "one tends to owe even more",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "17d": {
+        "word": "Chaos",
+        "clue": "what’s outside the picture is more important than what’s in, like a mountain carving space from time, time from space",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "18d": {
+        "word": "Colleague",
+        "clue": "a being with whom we labor in time, a higher bar than stranger or neighbor, who are less inconvenient to us",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "35d": {
+        "word": "Compassion",
+        "clue": "sharing passion, sharing passivity, sharing our common *inability*",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "24a": {
+        "word": "Consciousness",
+        "clue": "optional, the ability to choose between undecidable alternatives—self-perception, self-enjoyment, self-judgment, self-loathing",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "20d": {
+        "word": "Death",
+        "clue": "the child you chose not to have, a curious boy on a stone wall, kicking his legs",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "11a": {
+        "word": "Depression",
+        "clue": "the logged-off stumped fields of your mind",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "10d": {
+        "word": "Depression",
+        "clue": "flight inwards to where there is no sun",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "8d": {
+        "word": "Desire",
+        "clue": "the first layer of language that the stars carry",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "40a": {
+        "word": "Destiny",
+        "clue": "the best thing for you might be happening because I’m not there",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "12a": {
+        "word": "Disaffection and disillusionment",
+        "clue": "contagions we can spread ourselves",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "29a": {
+        "word": "Doubt",
+        "clue": "the child posed on the threshold of a door; also a ghost going the other way",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "40d": {
+        "word": "Duality",
+        "clue": "the 363 other days of shadowlands, a whole that is constantly in motion and constantly changing",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "9d": {
+        "word": "Emancipation",
+        "clue": "regulating behavior by drawing advice from what we see in the sun, moon, stars, wind, etc.",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "5d": {
+        "word": "Empathy",
+        "clue": "requires symmetry",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "47a": {
+        "word": "Ethics",
+        "clue": "empathic self-love that cannot be dissociated from the well-being of others",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    // edited?
+    "1d": {
+        "word": "Excessiveness",
+        "clue": "emancipation from established meaning by the revelation of the unseen horizon of the possible",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "42a": {
+        "word": "Faith",
+        "clue": "takes a practiced witness and I am neither",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "21a": {
+        "word": "Fate",
+        "clue": "like money or God which does not exist but is",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "22d": {
+        "word": "Fortune",
+        "clue": "the chaos hiding in the folds of the human mind",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "13a": {
+        "word": "Frankness",
+        "clue": "a form of being reliable",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "27a": {
+        "word": "Free will",
+        "clue": "depends on probability, between *can* and *do*",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "21d": {
+        "word": "Free will",
+        "clue": "imagination walking alongside history",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "25a": {
+        "word": "Grace",
+        "clue": "a condition of coincidence",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    // edited?
+    "45a": {
+        "word": "Grief",
+        "clue": "the felt and inarticulate sense of the out-of-jointedness or wrongness of the world as an irritant in the present",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "4d": {
+        "word": "Humility",
+        "clue": "awareness of the impermanence of our acts, our bodies, our consciousness",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "44a": {
+        "word": "Identity",
+        "clue": "not a set of conscious decisions but a set of compulsions, attachments and identifications that we feel",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    "2d": {
+        "word": "Identity",
+        "clue": "a ruse, based on misunderstanding, exclusion, and aggression",
+        "startX": 0,
+        "startY": 0,
+    },    
+    
+    // I can't find this word
+    // eventually found it in crossword but not clues
+    "43a": {
+        "word": "Intelligence",
+        "clue": "mandatory, choosing between decidable alternatives",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "37a": {
+        "word": "Innovation",
+        "clue": "think of it like weather rather than forward scientific progress",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "23a": {
+        "word": "Innuendo",
+        "clue": "laughing in our sunglasses, forgetting to look back",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "26d": {
+        "word": "Light bulb",
+        "clue": "makes the Ferris Wheel and nightshift possible",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    // I can't find this word
+    // eventually found it in crossword but not clues
+    "33a": {
+        "word": "Love and grief",
+        "clue": "lend themselves perhaps too readily to narrative",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "16d": {
+        "word": "Magic",
+        "clue": "an erotic relationship that is working",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "16a": {
+        "word": "Mischief",
+        "clue": "something wild vandalizing whatever the skull holds",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "46a": {
+        "word": "Poetry",
+        "clue": "reshuffles the boundaries between conscious and unconscious",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "28d": {
+        "word": "Sentimentality",
+        "clue": "the belief that underneath we are all alike",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    // I couldn't find this one
+    // eventually found it in crossword but not clues
+    "39a": {
+        "word": "Stars",
+        "clue": "thoughts",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "6d": {
+        "word": "The elaborate system of status",
+        "clue": "self replaces soul as the fist of survival",
+        "startX": 0,
+        "startY": 0,
+    },
+
+    "3d": {
+        "word": "The only enduring legacy of our species",
+        "clue": "nuclear waste, climate change, mass extinction",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "36d": {
+        "word": "To advise",
+        "clue": "to tell what we see",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "38a": {
+        "word": "To collaborate",
+        "clue": "requires seeing things together with no claim about harmony or intersubjectivity, like Sedgwick’s “white glasses,” focusing not on our feelings and fears but on what the scene is that we want to materialize and return to",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "3a": {
+        "word": "Tone",
+        "clue": "the action of the mind’s hands as they move through the air of the encounter",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    // edited
+    "7d": {
+        "word": "Trauma",
+        "clue": "we discover our profound not mattering to the shape our lives take, but have to live anyway in what has been revealed as an impersonal space that houses the fantasy that we organize our worlds",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "41a": {
+        "word": "Unconcious",
+        "clue": "like the depths of the ocean where even the imagination cannot dive",
+        "startX": 0,
+        "startY": 0,
+    },
+    
+    "32a": {
+        "word": "Wind",
+        "clue": "words",
+        "startX": 0,
+        "startY": 0,
+    }
+
+};
+
+// this is a guess. meant to be edited.
+export const dimensions = [50, 50];
