e6337b79e96e6c8fd8fbe192630f80e30351c450
Isaac Bake <isaac@blake.earth>
2020-04-21 14:55:30 -0400

    Add clues column without notifications/alerts

--- src/app/clues/clues.component.ts
+++ src/app/clues/clues.component.ts
@@ -7,11 +7,56 @@ import { ids } from '../yesterdaysNews'
   styleUrls: ['./clues.component.css']
 })
 export class CluesComponent implements OnInit {
-  ids = ids;
+  ids = ids; //what is the right type???
+  downIds: Array<String>;;
+  acrossIds: Array<String>;
 
-  constructor() { }
+  constructor() {
+    let idsKeys: Array<String> = Object.keys(ids);
+    let collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'}); //type it
+    idsKeys = idsKeys.sort(collator.compare);
+    this.acrossIds = new Array<String>();
+    this.downIds = new Array<String>();
+    idsKeys.forEach(key => {
+      if (key.charAt(key.length - 1) == 'a') {
+        this.acrossIds.push(key);
+      }
+      else {
+        this.downIds.push(key);
+      }
+    });
+   }
 
   ngOnInit(): void {
+  }
+
+  switch(event: Event): void {
+    let tab: Element = (event.target as Element);
+    if (!tab.classList.contains("selected") && tab.classList.contains("tab")) {
+      let currentSelectedTab: HTMLCollectionOf<Element> = document.getElementsByClassName("selected");
+      
+
+      let currentHiddenClues: HTMLCollectionOf<Element> = document.getElementsByClassName("hidden");
+      currentHiddenClues[0].classList.remove("hidden");
+
+      let currentSelectedClues: HTMLCollectionOf<Element>;
+      if (currentSelectedTab[0].id == "across") {
+        currentSelectedClues = document.getElementsByClassName("across");
+      }
+      else {
+        currentSelectedClues = document.getElementsByClassName("down");
+      }
+      currentSelectedClues[0].classList.add("hidden");
+
+      currentSelectedTab[0].classList.remove("selected");
+      tab.classList.add("selected");
+    }
+
+  }
+
+  trigger(id: String): void {
+    // alert parent
+    alert("will alert app with id " + id );
   }
 
 }
