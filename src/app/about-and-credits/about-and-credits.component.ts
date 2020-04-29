139c94871c00598770125754ac97789ffb2f8fc0
Daniel Dykiel <dana.dykiel@gmail.com>
2020-04-29 15:43:06 -0400

    Newspaper formatting A&C
    A few changes still remain to be made:
    - Check italics
    - Change button colors to match tabs
    Make sure font works once it's integrated!

--- src/app/about-and-credits/about-and-credits.component.ts
+++ src/app/about-and-credits/about-and-credits.component.ts
@@ -1,3 +1,7 @@
+/** An about and credits page with a newspaper aesthetic.
+Commented-out code (in .html and .css sections) represents changes that can be made to the layout.
+I'm keeping them here if Dr. Carr asks for layout changes. */
+
 import { Component, OnInit } from '@angular/core';
 
 @Component({
