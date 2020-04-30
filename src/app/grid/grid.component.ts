af8bb45142f0d082439406d3d678e039657651c7
Mei Maddox <maddoxamei@gmail.com>
2020-04-30 10:59:44 -0400

    Testing with Crossword

--- src/app/grid/grid.component.ts
+++ src/app/grid/grid.component.ts
@@ -1,15 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { ids } from '../yesterdaysNews'
+//import { get } from "scriptjs";
+declare var createTable: any;
+declare var finalize: any;
 
 @Component({
   selector: 'app-grid',
   templateUrl: './grid.component.html',
   styleUrls: ['./grid.component.css']
 })
+
 export class GridComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
+  ids = ids;
+  constructor() {
+    let idsKeys: Array<String> = Object.keys(ids);
+    let collator = new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'}); //type it
+    idsKeys = idsKeys.sort(collator.compare);
+    idsKeys.forEach(key => {
+      //alert(key.id);
+    });
   }
 
+  ngOnInit(): void {
+    createTable(40,40);
+  }
+
+  finalize(){
+    finalize();
+  }
 }
