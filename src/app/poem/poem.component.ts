7281cb355e4e9b500603660e6e7349cef83457b7
Hunter Chasens <hunter.chasens18@ncf.edu>
2020-05-06 19:07:15 -0400

    poem without clues file

--- src/app/poem/poem.component.ts
+++ src/app/poem/poem.component.ts
@@ -1,15 +1,35 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Input } from '@angular/core';
 
+
+/**
+ * This Componenet is responsable for displaying the final poem and is located on the bottom of the page
+ */
 @Component({
   selector: 'app-poem',
   templateUrl: './poem.component.html',
+  template: `{{ message }}`,
   styleUrls: ['./poem.component.css']
 })
 export class PoemComponent implements OnInit {
 
-  constructor() { }
+  //The string that contains the poem (decleration)
+  poemText:String;
+
+  @Input() childMessage: string;
+
+  constructor() {
+    //defines poemText
+    this.poemText = new String();
+  }
 
   ngOnInit(): void {
+    this.addClue("testOfClueAddFunction");
   }
+
+  addClue(text:String){
+    this.poemText+= "\n" + text.toString();
+  }
+
+
 
 }
