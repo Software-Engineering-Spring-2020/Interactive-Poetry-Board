176b56873f7e92bc62f3456aae5b2f85279775dd
maddoxamei <maddoxamei@gmail.com>
2020-05-06 23:48:19 -0400

    Tricky Questions Poem

--- src/assets/ts/index.ts
+++ src/assets/ts/index.ts
@@ -1,7 +1,8 @@
 import * as YN from "./yesterdaysNews";
+import * as TQ from "./trickyQuestions";
 import * as T from "./test";
 
 
-export const ids = [YN.ids, T.ids];
-export const dimensions = [YN.dimensions, T.dimensions];
-export const titles = [YN.title, T.title];
+export const ids = [YN.ids, TQ.ids, T.ids];
+export const dimensions = [YN.dimensions, TQ.dimensions, T.dimensions];
+export const titles = [YN.title, TQ.title, T.title];
---
+++ src/assets/ts/trickyQuestions.ts
@@ -0,0 +1,364 @@
+// Rows, Columns
+export const dimensions = [43, 43];
+
+export const title = "Tricky Questions";
+
+export const ids = {
+"8a": {
+    "word": "The fountain of youth",
+    "clue": "is real good drinking water, anyhow",
+    "startX": 15,
+    "startY": 3
+},
+
+"9a": {
+    "word": "A sharp shooter",
+    "clue": "a woman clean of received wisdom and open to what she wants",
+    "startX": 0,
+    "startY": 4
+},
+
+"11a": {
+    "word": "Sunlight",
+    "clue": "is hard to paint",
+    "startX": 0,
+    "startY": 6
+},
+
+"12a": {
+    "word": "Birds",
+    "clue": "have their own ideas about monogamy",
+    "startX": 18,
+    "startY": 6
+},
+
+"14a": {
+    "word": "The real war",
+    "clue": "is the impossible demand that men make on women and vice versa",
+    "startX": 28,
+    "startY": 7
+},
+
+"15a": {
+    "word": "Procedural memory",
+    "clue": "your body never forgets as a matter of course",
+    "startX": 5,
+    "startY": 8
+},
+
+"17a": {
+    "word": "The power of work",
+    "clue": "transforming the world, objectifying intelligence, producing subjectivity",
+    "startX": 3,
+    "startY": 10
+},
+
+"18a": {
+    "word": "Sea hares, whales, and sea pups",
+    "clue": "I kid you not, they’ll never forget!",
+    "startX": 0,
+    "startY": 12
+},
+
+"19a": {
+    "word": "Your reproductive capital",
+    "clue": "is $7,000",
+    "startX": 10,
+    "startY": 15
+},
+
+"23a": {
+    "word": "The chance that mimics choice",
+    "clue": "the flaw that makes the flower",
+    "startX": 4,
+    "startY": 18
+},
+
+"24a": {
+    "word": "Memory and imagination",
+    "clue": "have to be archived separately, otherwise we couldn’t go on living",
+    "startX": 0,
+    "startY": 20
+},
+
+"25a": {
+    "word": "Garage Sale",
+    "clue": "a metaphor for the mind",
+    "startX": 17,
+    "startY": 22
+},
+
+"26a": {
+    "word": "Destiny",
+    "clue": "the permission to make mistakes",
+    "startX": 35,
+    "startY": 22
+},
+
+"27a": {
+    "word": "In a fairytale",
+    "clue": "you ask for what you desire",
+    "startX": 0,
+    "startY": 23
+},
+
+"28a": {
+    "word": "Plant sex",
+    "clue": "has a surrealist efficacy",
+    "startX": 28,
+    "startY": 24
+},
+
+"29a": {
+    "word": "Extraterrestrial life",
+    "clue": "is by definition soothing",
+    "startX": 1,
+    "startY": 25
+},
+
+"30a": {
+    "word": "The self",
+    "clue": "accumulates in the manner of a tree",
+    "startX": 34,
+    "startY": 26
+},
+
+"33a": {
+    "word": "She never puts on weight",
+    "clue": "you’ll notice it’s often true of selfish women",
+    "startX": 12,
+    "startY": 28
+},
+
+"34a": {
+    "word": "The bluebird way and the dandelion way",
+    "clue": "the two ways to escape",
+    "startX": 2,
+    "startY": 30
+},
+
+"35a": {
+    "word": "Saying sorry",
+    "clue": "is one measure of intelligence",
+    "startX": 2,
+    "startY": 32
+},
+
+"36a": {
+    "word": "Trees, boars, and snakes",
+    "clue": "may provide good fortune, but they also require compensation",
+    "startX": 16,
+    "startY": 32
+},
+
+"37a": {
+    "word": "What the tiger is to space",
+    "clue": "the sex act is to time",
+    "startX": 6,
+    "startY": 34
+},
+
+"39a": {
+    "word": "How one eats",
+    "clue": "speaks to issues of basic character, for women in particular",
+    "startX": 0,
+    "startY": 36
+},
+
+"42a": {
+    "word": "Episodic memory",
+    "clue": "what exactly you said to your lover when you met him in the forest",
+    "startX": 26,
+    "startY": 36
+},
+
+"43a": {
+    "word": "Taco Bells",
+    "clue": "seem to rob everything of its perspective",
+    "startX": 0,
+    "startY": 38
+},
+
+"44a": {
+    "word": "In the afternoon",
+    "clue": "the soul gets smaller",
+    "startX": 15,
+    "startY": 38
+},
+
+"45a": {
+    "word": "Beauty",
+    "clue": "is the kind of tunnel vision you need",
+    "startX": 33,
+    "startY": 38
+},
+
+"46a": {
+    "word": "Climax",
+    "clue": "ties to the afterlife, our profound longing for sanctuary, a claim on the future",
+    "startX": 3,
+    "startY": 40
+},
+
+"47a": {
+    "word": "Destiny",
+    "clue": "a pre-marital investment",
+    "startX": 13,
+    "startY": 40
+},
+
+"48a": {
+    "word": "Style",
+    "clue": "is how you respond in a crisis",
+    "startX": 20,
+    "startY": 41
+},
+
+"49a": {
+    "word": "Chivalry",
+    "clue": "shapes women as a horizon of anticipation",
+    "startX": 31,
+    "startY": 41
+},
+
+"50a": {
+    "word": "writing",
+    "clue": "only humans can do this, experiencing something separately together",
+    "startX": 1,
+    "startY": 42
+},
+
+"51a": {
+    "word": "Freedom",
+    "clue": "is a question astronauts ask of the stars",
+    "startX": 11,
+    "startY": 42
+},
+
+"1d": {
+    "word": "Genius",
+    "clue": "like someone with mental illness only more so",
+    "startX": 23,
+    "startY": 0
+},
+
+"2d": {
+    "word": "What is love",
+    "clue": "if not defined in relation to the loss of it",
+    "startX": 25,
+    "startY": 0
+},
+
+"3d": {
+    "word": "Isn’t every piece of luck",
+    "clue": "a trap in disguise",
+    "startX": 27,
+    "startY": 0
+},
+
+"4d": {
+    "word": "The moral",
+    "clue": "is at the mercy of the blocks of fiction it encounters as it goes along",
+    "startX": 40,
+    "startY": 0
+},
+
+"5d": {
+    "word": "An atmosphere of happiness",
+    "clue": "surrounds large trees",
+    "startX": 8,
+    "startY": 1
+},
+
+"6d": {
+    "word": "work",
+    "clue": "crafts identities and capability to answer for, to answer to",
+    "startX": 29,
+    "startY": 1
+},
+
+"7d": {
+    "word": "Your reproductive capital",
+    "clue": "would turn out to be harder to use than it was to win",
+    "startX": 33,
+    "startY": 1
+},
+
+"10d": {
+    "word": "Starling flight",
+    "clue": "a model for political action",
+    "startX": 35,
+    "startY": 4
+},
+
+"13d": {
+    "word": "The separation of love and passion",
+    "clue": "gives the French access to both",
+    "startX": 30,
+    "startY": 5
+},
+
+"16d": {
+    "word": "Even nature",
+    "clue": "has a male gaze",
+    "startX": 2,
+    "startY": 9
+},
+
+"20d": {
+    "word": "Semantic memory",
+    "clue": "an emerging galaxy in the agony of information",
+    "startX": 40,
+    "startY": 15
+},
+
+"21d": {
+    "word": "Destiny",
+    "clue": "the result of a purposeless natural process that does not have you in mind",
+    "startX": 42,
+    "startY": 15
+},
+
+"22d": {
+    "word": "Nature",
+    "clue": "tells the body stories that are true",
+    "startX": 37,
+    "startY": 16
+},
+
+"31d": {
+    "word": "Hope",
+    "clue": "goes something like this — we swore allegiance",
+    "startX": 10,
+    "startY": 26
+},
+
+"32d": {
+    "word": "Every good plot",
+    "clue": "is a cliché ",
+    "startX": 5,
+    "startY": 27
+},
+
+"38d": {
+    "word": "Free will",
+    "clue": "is not consistent with the laws of physics",
+    "startX": 37,
+    "startY": 33
+},
+
+"40d": {
+    "word": "An -ism",
+    "clue": "not a cause but a symptom",
+    "startX": 18,
+    "startY": 35
+}/*,
+
+"41d": {
+    "word": "A reverse fountain of youth",
+    "clue": "the kind of tunnel vision you need",
+    "startX": 23,
+    "startY": 35
+}*/
+
+};
