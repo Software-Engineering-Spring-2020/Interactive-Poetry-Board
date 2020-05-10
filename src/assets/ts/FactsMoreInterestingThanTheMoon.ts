46db1b52bfce8b4b0c8ce52cbb07a6bcca8089da
Daniel Dykiel <dana.dykiel@gmail.com>
2020-05-10 16:56:43 -0400

    Add down clues / edit dimensions

--- src/assets/ts/FactsMoreInterestingThanTheMoon.ts
+++ src/assets/ts/FactsMoreInterestingThanTheMoon.ts
@@ -1,7 +1,266 @@
 // Rows, Columns
-export const dimensions = [50, 50];
+export const dimensions = [43, 43];
 
 export const title = "Facts More Interesting Than The Moon";
 
 export const ids = {
+  "1d": {
+      "word": "ADAM",
+      "clue": "God dismembered himself & waited for the fragments to spark.",
+      "startX": 2,
+      "startY": 0
+  },
+  "2d": {
+      "word": "MANATEE",
+      "clue": "has no use in the modern world except as myth",
+      "startX": 13,
+      "startY": 0
+  },
+  "3d": {
+      "word": "ENDING",
+      "clue": "In the garden the Tree of Knowledge was the last to come to leaf.",
+      "startX": 15,
+      "startY": 0
+  },
+  "4d": {
+      "word": "BIG UTOPIAN LONELY THINKING PARADISIACAL",
+      "clue": "Einstein for example taking life—dogs, calves, a horse, an elephant—in the belief the ends justified the means",
+      "startX": 32,
+      "startY": 0
+  },
+  "5d": {
+      "word": "SPECIE",
+      "clue": "shit & gold, filthy lucre; a red-haired buffalo, an extinct pheasant, relocated Canadian wolves & upscale Slovakian bears",
+      "startX": 40,
+      "startY":
+  },
+  "8d": {
+      "word": "PARADISE",
+      "clue": "seeing yourself as fragments of who you have been—",
+      "startX": 0,
+      "startY":
+  },
+  "9d": {
+      "word": "LOVE",
+      "clue": "inside the poem, the memory of roots",
+      "startX": 19,
+      "startY": 2
+  },
+  "10d": {
+      "word": "PARADISE",
+      "clue": "a moment poised before an inevitable after—",
+      "startX": 38,
+      "startY": 2
+  },
+  "11d": {
+      "word": "HE HANDLED MY BREASTS",
+      "clue": "swallowing the curve of a muscle in the fist of <i>yes</i>",
+      "startX": 10,
+      "startY": 3
+  },
+  "12d": {
+      "word": "ONTOLOGICAL CHOREOGRAPHIES",
+      "clue": "just <i>who</i> is at home must be permanently in question",
+      "startX": 23,
+      "startY": 3
+  },
+  "15d": {
+      "word": "THE SPACTACLE OF A NATURAL DEATH",
+      "clue": "the pheasant, who dies of a heart attack",
+      "startX": 1,
+      "startY": 6
+  },
+  "16d": {
+      "word": "AS LUCK WOULD HAVE IT",
+      "clue": "the leaf may also listen—",
+      "startX": 8,
+      "startY": 7
+  },
+  "17d": {
+      "word": "MY OLD DOPPELGÄNGER",
+      "clue": "cyborgs doing feminist work in Reagan-era Star Wars",
+      "startX": 16,
+      "startY": 7
+  },
+  "18d": {
+      "word": "FEAR",
+      "clue": "is the butcher knife",
+      "startX": 18,
+      "startY": 7
+  },
+  "19d": {
+      "word": "TO DARE & TO LOVE",
+      "clue": "what you gain through sacrifice",
+      "startX": 5,
+      "startY": 8
+  },
+  "20d": {
+      "word": "L",
+      "clue": "at a certain point your body is no longer your own",
+      "startX": 3,
+      "startY": 9
+  },
+  "21d": {
+      "word": "MANMADE LANGUAGE",
+      "clue": "words you swallow every day, & attempt to make your own",
+      "startX": 41,
+      "startY": 9
+  },
+  "22d": {
+      "word": "QUOTIDIAN PAROLE",
+      "clue": "The Word thought <i>blood.</i> The Word thought <i>walk.</i> The Word went dark.",
+      "startX": 3,
+      "startY": 11
+  },
+  "23d": {
+      "word": "A SURROGACY AGREEMENT",
+      "clue": "requires recognizing one cannot know the other or the self",
+      "startX": 19,
+      "startY": 12
+  },
+  "24d": {
+      "word": "HERON IN A RUINED LANDSCAPE",
+      "clue": "white as robed angels must be, beating his head against a tree",
+      "startX": 12,
+      "startY": 13
+  },
+  "25d": {
+      "word": "REDEMPTION VS DELIVERANCE",
+      "clue": "like nudists on the moon; we always have the chance to reinvent ourselves",
+      "startX": 14,
+      "startY": 13
+  },
+  "26d": {
+      "word": "SEX",
+      "clue": "an urgency that can’t be bothered with &/or isn’t allowed verbs",
+      "startX": 30,
+      "startY": 13
+  },
+  "28d": {
+      "word": "AM I BLUE",
+      "clue": "eating misery—has its flip side, which also has its flip side & so on ad infinitum",
+      "startX": 25,
+      "startY": 16
+  },
+  "30d": {
+      "word": "KINGDOM",
+      "clue": "when God invented predators",
+      "startX": 21,
+      "startY": 20
+  },
+  "31d": {
+      "word": "CRUCIFIED TO PARADOX",
+      "clue": "the failure that made the sacrifice necessary",
+      "startX": 37,
+      "startY": 20
+  },
+  "32d": {
+      "word": "SELF-POSSESSION",
+      "clue": "You became yourself, & were haunted.",
+      "startX": 39,
+      "startY": 20
+  },
+  "33d": {
+      "word": "PENURY",
+      "clue": "…living in perfect accord with the values of our time",
+      "startX": 27,
+      "startY": 23
+  },
+  "34d": {
+      "word": "MITOCHONDRIAL EVE",
+      "clue": "full of snakes, full of trellises, full of indirection",
+      "startX": 34,
+      "startY": 24
+  },
+  "35d": {
+      "word": "VERSUS",
+      "clue": "like a tidal wave or the sun setting, you just surrender",
+      "startX": 4,
+      "startY": 26
+  },
+  "36d": {
+      "word": "ENVIRONMENT",
+      "clue": "a lost word, you don't mind giving it up",
+      "startX": 42,
+      "startY": 26
+  },
+  "40d": {
+      "word": "LOVE",
+      "clue": "kills, unconditionally, both kinds & individuals",
+      "startX": 25,
+      "startY": 30
+  },
+  "42d": {
+      "word": "DEATH",
+      "clue": "moves like wind through canyons, cutting down mountains & making space for God",
+      "startX": 7,
+      "startY": 31
+  },
+  "44d": {
+      "word": "LOVE",
+      "clue": "what most of us don't even know we don't know how to do",
+      "startX": 30,
+      "startY": 34
+  },
+  "45d": {
+      "word": "EASEL",
+      "clue": "from ass: to support, to sustain, to give courage",
+      "startX": 20,
+      "startY": 35
+  },
+  "46d": {
+      "word": "TRAUMA",
+      "clue": "the meat of the mountain slides down her bones",
+      "startX": 24,
+      "startY": 35
+  },
+  "47d": {
+      "word": "PRAISE",
+      "clue": "a songline from the source to the sea",
+      "startX": 28,
+      "startY": 35
+  },
+  "48d": {
+      "word": "FEAR",
+      "clue": "the window on which licks the night",
+      "startX": 6,
+      "startY": 36
+  },
+  "49d": {
+      "word": "WILDER",
+      "clue": "a muzzled dog chained to The Tree of Knowledge, fulfilling her metaphors",
+      "startX": 8,
+      "startY": 36
+  },
+  "50d": {
+      "word": "FLINCH",
+      "clue": "the swamp grows, the mountain slows; the mountain rises, the swamp shrinks",
+      "startX": 26,
+      "startY": 36
+  },
+  "51d": {
+      "word": "EDGES",
+      "clue": "The mind makes a shrine of what it can’t understand, the way a shipwreck turns to treasure as soon as it is submerged in the sea.",
+      "startX": 39,
+      "startY": 36
+  },
+  "52d": {
+      "word": "LIFE",
+      "clue": "what God drowned inside the sea",
+      "startX": 22,
+      "startY": 37
+  },
+  "53d": {
+      "word": "FATE",
+      "clue": "There never was an end. We kept right on, failing.",
+      "startX": 2,
+      "startY": 38
+  },
+  "54d": {
+      "word": "DOGS",
+      "clue": "partners in the crime of human evolution",
+      "startX": 42,
+      "startY": 38
+  },
+
 };
