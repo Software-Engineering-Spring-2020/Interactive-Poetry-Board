d8da9d182aab5b81347d8b539661c6ae0615b96d
Isaac Bake <isaac@blake.earth>
2020-05-11 00:35:57 -0400

    Build changes

--- docs/assets/ts/FactsMoreInterestingThanTheMoon.ts
+++ docs/assets/ts/FactsMoreInterestingThanTheMoon.ts
@@ -28,18 +28,18 @@ export const ids = {
       "startX": 32,
       "startY": 0
   },
-  /*"5d": {
+  "5d": {
       "word": "SPECIE",
       "clue": "shit & gold, filthy lucre; a red-haired buffalo, an extinct pheasant, relocated Canadian wolves & upscale Slovakian bears",
       "startX": 40,
-      "startY":
+      "startY": 0
   },
   "8d": {
       "word": "PARADISE",
       "clue": "seeing yourself as fragments of who you have been—",
-      "startX": 0,
-      "startY":
-  },*/
+      "startX": 4,
+      "startY": 2
+  },
   "9d": {
       "word": "LOVE",
       "clue": "inside the poem, the memory of roots",
@@ -261,5 +261,269 @@ export const ids = {
       "clue": "partners in the crime of human evolution",
       "startX": 42,
       "startY": 38
+  },
+  "6a": {
+    "word": "ALIBI",
+    "clue": "an intimate understanding of failure",
+    "startX": 1,
+    "startY": 1
+  },
+  "7a": {
+    "word": "ECSTASY",
+    "clue": "sailing slowly over the undreamed of abyss",
+    "startX": 14,
+    "startY": 1
+  },
+  "8a": {
+    "word": "POSITIVE BONDAGE",
+    "clue": "Calm down. What happens. Happens mostly without you.",
+    "startX": 22,
+    "startY": 1
+  },
+  "11a": {
+    "word": "LAMB",
+    "clue": "ribs, loin, shank, leg & , occasionally, a savior",
+    "startX": 18,
+    "startY": 3
+  },
+  "12a": {
+    "word": "MEATY",
+    "clue": "as a fox, as a bear, as a philly",
+    "startX": 1,
+    "startY": 4
+  },
+  "13a": {
+    "word": "EATING",
+    "clue": "the lion sticks out her tongue, takes the lamb, takes the sun right into her being",
+    "startX": 11,
+    "startY": 4
+  },
+  "14a": {
+    "word": "SPECIES ARROGANCE",
+    "clue": "Gloria Steinem eating veal, Hélène Cixous in an ermine coat",
+    "startX": 24,
+    "startY": 4
+  },
+  "15a": {
+    "word": "THAT THE EVERGLADES STILL EXIST",
+    "clue": "a collective illusion shared by both those who care & those who don't",
+    "startX": 1,
+    "startY": 6
+  },
+  "16a": {
+    "word": "EVE",
+    "clue": "the first witch, who sees right through it",
+    "startX": 39,
+    "startY": 6
+  },
+  "18a": {
+    "word": "SLIPSHAPE",
+    "clue": "the snake in the middle of her shedding.",
+    "startX": 21,
+    "startY": 8
+  },
+  "19a": {
+    "word": "POLITICS",
+    "clue": "is done to the body",
+    "startX": 34,
+    "startY": 8
+  },
+  "20a": {
+    "word": "STARS",
+    "clue": "one day the gods stepped back inside themselves & no one could follow",
+    "startX": 3,
+    "startY": 9
+  },
+  "21a": {
+    "word": "LEO",
+    "clue": "the first lion, who slept in God’s shadow",
+    "startX": 2,
+    "startY": 10
+  },
+  "22a": {
+    "word": "LIKE HAMBURGER YOU MAY",
+    "clue": "the cow prepares herself in a variety of ways, and so must you",
+    "startX": 11,
+    "startY": 10
+  },
+  "23a": {
+    "word": "PUBERTY",
+    "clue": "blind verbs without a subject, we hardly knew what was happening...",
+    "startX": 6,
+    "startY": 12
+  },
+  "24a": {
+    "word": "ECOLOGICAL ERASURE",
+    "clue": "the bird of love & the bird of fear flying to the place where the forest should have been—",
+    "startX": 21,
+    "startY": 12
+  },
+  "25a": {
+    "word": "DREAM",
+    "clue": "what the drowned god gifted",
+    "startX": 15,
+    "startY": 13
+  },
+  "26a": {
+    "word": "THE STORY OF MEAT",
+    "clue": "how the bison got to Florida, & why they stayed—",
+    "startX": 25,
+    "startY": 14
+  },
+  "27a": {
+    "word": "SILO",
+    "clue": "is undefined even the shouting accusers won’t cross that line",
+    "startX": 18,
+    "startY": 15
+  },
+  "28a": {
+    "word": "EXPERIMENT",
+    "clue": "listen out, breathe in",
+    "startX": 28,
+    "startY": 16
+  },
+  "29a": {
+    "word": "ECHO",
+    "clue": "another variation of extinction",
+    "startX": 15,
+    "startY": 19
+  },
+  "30a": {
+    "word": "DRY AUGUST BURNED",
+    "clue": "just wars, a rabbit to be skinned",
+    "startX": 21,
+    "startY": 19
+  },
+  "32a": {
+    "word": "THE DAUNTING TASK OF OENERSHIP",
+    "clue": "unconditional love demands cooperation of two and humiliation of the one",
+    "startX": 4,
+    "startY": 21
+  },
+  "33a": {
+    "word": "ECONOMY",
+    "clue": "is the bone",
+    "startX": 28,
+    "startY": 23
+  },
+  "34a": {
+    "word": "A RULE",
+    "clue": "the sacred trumpets demanded meat",
+    "startX": 0,
+    "startY": 24
+  },
+  "35a": {
+    "word": "UNKONWN",
+    "clue": "The only question I didn’t ask was whose imagination was I in.",
+    "startX": 25,
+    "startY": 26
+  },
+  "36a": {
+    "word": "EVE",
+    "clue": "talks to fish, our first ancestors",
+    "startX": 2,
+    "startY": 27
+  },
+  "37a": {
+    "word": "SPIN",
+    "clue": "where intellect & emotion transfer power",
+    "startX": 9,
+    "startY": 27
+  },
+  "38a": {
+    "word": "RECIPROCAL POSSESSION",
+    "clue": "the ocean washing from shore to shore, changing her name",
+    "startX": 21,
+    "startY": 28
+  },
+  "39a": {
+    "word": "SCENERY",
+    "clue": "gets used; a road here, a few trees there, a chink of light through a dead leaf—",
+    "startX": 9,
+    "startY": 29
+  },
+  "40a": {
+    "word": "SPARK",
+    "clue": "a lake on fire, a woman tapering back into a fish, stars slowing the night sky down",
+    "startX": 3,
+    "startY": 30
+  },
+  "41a": {
+    "word": "EMPATHY",
+    "clue": "the tide inside woman that moves her to her moon",
+    "startX": 28,
+    "startY": 30
+  },
+  "42a": {
+    "word": "THE PROBLEM",
+    "clue": "there are too many protagonists in this story",
+    "startX": 16,
+    "startY": 31
+  },
+  "43a": {
+    "word": "LOSS",
+    "clue": "two albatrosses on the battlefield of love",
+    "startX": 0,
+    "startY": 32
+  },
+  "44a": {
+    "word": "THE GREATEST STORY EVER TOLD",
+    "clue": "instead of trying to prove her point, she tried to prove herself wrong",
+    "startX": 4,
+    "startY": 33
+  },
+  "45a": {
+    "word": "ALARM",
+    "clue": "a piano duet in which the birds worry",
+    "startX": 0,
+    "startY": 36
+  },
+  "49a": {
+    "word": "ABSENT REFERENT",
+    "clue": "both there and not here, undercut as it is absorbed—",
+    "startX": 9,
+    "startY": 36
+  },
+  "50a": {
+    "word": "PRODIGAL",
+    "clue": "my animal came back to tell me what it was like without me...",
+    "startX": 27,
+    "startY": 36
+  },
+  "51a": {
+    "word": "FATE",
+    "clue": "Whatever you did not recognize in time. I mean really why. I mean.",
+    "startX": 35,
+    "startY": 37
+  },
+  "53a": {
+    "word": "METIS GAVE COUNSEL",
+    "clue": "from the belly of Zeus",
+    "startX": 4,
+    "startY": 38
+  },
+  "55a": {
+    "word": "GOOD",
+    "clue": "& God said, <i>let there be wars</i>. World wars. & there were wars. & God saw the wars. & they were—",
+    "startX": 38,
+    "startY": 39
+  },
+  "56a": {
+    "word": "WAR",
+    "clue": "the desire for cows",
+    "startX": 3,
+    "startY": 40
+  },
+  "57a": {
+    "word": "THE LIBERAL FEMINIST",
+    "clue": "that human trying to throw her stick to his dog only—",
+    "startX": 9,
+    "startY": 40
+  },
+  "58a": {
+    "word": "METAPHOR",
+    "clue": "just as <i>blue</i> is elegy for <i>sky</i>",
+    "startX": 0,
+    "startY": 42
   }
 };
